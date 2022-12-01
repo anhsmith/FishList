835b6d8d3e3637959ab90c60c42f583c90bf7cef
Adam Smith <anhsmith@gmail.com>
2022-12-01 20:26:46 +1300

    Compile new list from DuffyNZReefFish repo

---
+++ FishListScript.R
@@ -0,0 +1,43 @@
+
+library(rfishbase)
+Sys.setenv(FISHBASE_API="sealifebase")
+
+library(dplyr)
+
+# Download to tempfile
+github_link <- "https://github.com/anhsmith/DuffyNZReefFish/raw/master/DuffyNZReefFishMASTER.xlsx"
+library(httr)
+temp_file <- tempfile(fileext = ".xlsx")
+req <- GET(github_link,
+           # authenticate using GITHUB_PAT
+           authenticate(Sys.getenv("GITHUB_PAT"), ""),
+           # write result to disk
+           write_disk(path = temp_file))
+
+# Import
+tab <- readxl::read_excel(temp_file, sheet = "SpeciesInfo", col_names = T) |>
+  select(`!Family` = Family,
+         Genus,
+         Species,
+         CommonName = commonname,
+         Code = `...1`,
+         Size = `Size(mm)`,
+         MinDepth,
+         MaxDepth,
+         commonname,
+         TaxonomicNotes )
+
+# Write
+write.table(tab, file = "MasseySpeciesList.txt")
+
+# # Check names
+# vnfb <- validate_names(tab$genusspecies, server = "fishbase")
+#
+# cbind(tab$genusspecies,
+#       vnfb,
+#       tab$genusspecies==vnfb), file = "outfish.csv")
+#
+
+
+
+
